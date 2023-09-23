a2e8b2b193eecb6bfccc8ef06009d51a45298ebe
Prashant Sathawane <prashantsathawane2099@gmail.com>
2023-09-23 17:37:45 +0530

    accounts page

--- airbnb-project/src/App.jsx
+++ airbnb-project/src/App.jsx
@@ -3,17 +3,30 @@ import './App.css'
 import IndexPage from './pages/IndexPage'
 import LoginPage from './pages/LoginPage'
 import Layout from './Layout'
+import RegisterPage from './pages/RegisterPage'
+import axios from 'axios'
+import { UserContextProvider } from './UserContext'
+import { useEffect } from 'react'
+import { AccountPage } from './pages/AccountPage'
+
+axios.defaults.baseURL = "http://127.0.0.1:8080"
+axios.defaults.withCredentials = true;
 
 function App() {
 
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Route>
 
-    </Routes>
+  return (
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account/:subpage?" element={<AccountPage />} />
+          <Route path="/account/:subpage/:action" element={<AccountPage />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
 
   )
 }
